dd7185430c1afe9f7931c0a1fcf181a23ad4824d
shazy89 <erdo.shazy123@gmail.com>
2020-09-12 19:49:49 -0400

    add Materialize class

---
+++ models/materialize.js
@@ -0,0 +1,26 @@
+class Materialize {
+    static materialize(){
+      let trigger = new Materialize
+      trigger.modal()
+      trigger.sideNav()
+      trigger.collabsable()
+    }
+
+     modal(){
+        var elems = document.querySelectorAll('.modal');
+        return  M.Modal.init(elems, {});
+   
+      }
+  
+       sideNav() {
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems, {});
+        return instances
+      }
+  
+      collabsable() {
+        var elems = document.querySelectorAll('.collapsible');
+        var instances = M.Collapsible.init(elems, {});
+        return instances
+      }
+}
