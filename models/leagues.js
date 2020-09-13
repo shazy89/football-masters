af7de11c8728d9bf9d85783159c3c6d9c1c05258
shazy89 <erdo.shazy123@gmail.com>
2020-09-13 16:34:56 -0400

    get leagues data and display leagues data

--- models/leagues.js
+++ models/leagues.js
@@ -1,8 +1,9 @@
 class Leagues {
     static all =[];
+    static sortedAll =[]
 
-    constructor(id, name, country, country_code, logo, flag){
-       this.id = id
+    constructor(league_id, name, country, country_code, logo, flag){
+       this.league_id = league_id
        this.name = name
        this.country = country
        this.country_code = country_code
@@ -10,12 +11,53 @@ class Leagues {
        this.flag = flag
     }
 
-static createLeague(id, name, country, country_code, logo, flag) {
-    let league = new League(id, name, country, country_code, logo, flag)
-    Leagues.all.push(league)
-    return league
+static createLeague(league_id, name, country, country_code, logo, flag) {
+ //   let league = new Leagues(league_id, name, country, country_code, logo, flag)
+ //   Leagues.all.push(league)
+ //   league.display()
+ //   return league
+ debugger
 }
 
-static createLeagues(data){}
+static createLeagues(leaguesData){
+   let league = leaguesData.api.leagues.forEach(data => Leagues.createLeague(data.league_id, data.name, data.country, data.country_code, data.logo, data.flag))
+   return league
+}
 
+display() {
+    const collapsableUl = () => document.querySelector('.collapsible')
+    const li = document.createElement('li')
+    const div1 = document.createElement('div')
+    const flag = document.createElement('img')
+
+    const div2 = document.createElement('div')
+    const span = document.createElement('span')
+
+    collapsableUl().appendChild(li)
+
+    li.appendChild(div1)
+    li.appendChild(div2)
+
+    div1.appendChild(flag)
+    div1.className = "collapsible-header"
+    flag.src = this.flag
+    flag.style= "width: 30px; height: 20px; margin: 15px;"
+    div1.innerText = this.country
+
+    div2.appendChild(span)
+    div2.className = "collapsible-body"
+    span.innerText = "GOOD"
+
+}
+
+
+    
+
+
+
+//Leagues.all.map(element => {return element.country}).includes("Turkey")
+
+
+
+ 
 }
--- services/api.js
+++ services/api.js
@@ -1,8 +1,9 @@
 class Api {
-static countries = []
 
-   loadLeagues(){
-    fetch("https://api-football-v1.p.rapidapi.com/v2/leagues",{
+static allCountrieNames = []
+
+ static loadLeagues(){
+    fetch("https://api-football-v1.p.rapidapi.com/v2/countries",{
         method: "GET",
         headers: {
             "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
@@ -11,30 +12,38 @@ static countries = []
         }
       })
         .then(resp => resp.json())
-        .then(data => {} )
+        .then(data => Api.getCountries(data) )
 }
-   static getCountries() {
-       
+   static getCountries(data) {
+       data.api.countries.map(element => {Api.allCountrieNames.push({country: element.country}) 
+     })
    }
+
+     //${countryName.country}
+  static  leagues() {
+      for (let countryName of Api.allCountrieNames) {
+    fetch(`https://api-football-v1.p.rapidapi.com/v2/leagues/country/${countryName.country}/2020`,{
+      method: "GET",
+      headers: {
+          "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+          "x-rapidapi-key": "145adf9b47msh8117898cad08650p129e6bjsn989da873df13",
+          "useQueryString": true
+      }
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        let league = Leagues.createLeagues(data)
+      })
+    }
+   }
+
+  
+}
+
+
         
      
 
-    leagues() {
-   // for (let everyCountry of countTry) {
-   fetch(`https://api-football-v1.p.rapidapi.com/v2/leagues/current/england`,{
-     method: "GET",
-     headers: {
-         "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-         "x-rapidapi-key": "145adf9b47msh8117898cad08650p129e6bjsn989da873df13",
-         "useQueryString": true
-     }
-   })
-     .then(resp => resp.json())
-     .then(data => //addLeagues(data) 
-     {debugger})
-   }
-}
 
- //}
 
 // data.api.leagues.slice(0, 5)
